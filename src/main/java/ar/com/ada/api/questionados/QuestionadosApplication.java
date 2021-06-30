f608d8d1845db0f96d6afdfccebb1c79c4939c2d
Pzacarias <pzacarias.4@gmail.com>
2021-06-30 19:20:20 -0300

    Descripcion del juego.

--- src/main/java/ar/com/ada/api/questionados/QuestionadosApplication.java
+++ src/main/java/ar/com/ada/api/questionados/QuestionadosApplication.java
@@ -3,6 +3,8 @@ package ar.com.ada.api.questionados;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Aplicación tipo juego preguntados de preguntas y respuestas.
+
 @SpringBootApplication
 public class QuestionadosApplication {
 
